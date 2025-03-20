96f955cf4348f9f79868de861a856ec8e45fe344
kelsonalmeida <kelson.victr@gmail.com>
2025-03-20 20:19:16 -0300

    HomeScreen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import LoginScreen from './src/screens/LoginScreen'
+import HomeScreen from './src/screens/HomeScreen'
 
 const App = () => {
   return (
     
-      <LoginScreen />
+      <HomeScreen />
     
   )
 }
